a0bd84c7015d021ca08fc3b786ceda49e374a65f
stefano28 <stefanoleggio28@gmail.com>
2021-05-10 12:11:00 +0200

    list layout

--- app/src/main/java/com/example/jet_bring/ui/liste/ListeScreen.kt
+++ app/src/main/java/com/example/jet_bring/ui/liste/ListeScreen.kt
@@ -1,50 +1,78 @@
 package com.example.jet_bring.ui.liste
 
+import android.graphics.drawable.Icon
+import android.view.Gravity
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun ListeScreen() {
-    Text(text = "Liste",
-        style = TextStyle(color = MaterialTheme.colors.onBackground, fontSize = 36.sp),
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.background))
     LazyColumn {
-
         // Add a single item
         item {
-            Text(
-                text = "First item",
-                style = TextStyle(color = MaterialTheme.colors.onBackground, fontSize = 36.sp),
-            )
-
-        }
-
-        // Add 5 items
-        items(5) { index ->
-            Text(
-                text = "Item: $index",
-                style = TextStyle(color = MaterialTheme.colors.onBackground, fontSize = 36.sp),
-            )
-        }
-
-        // Add another single item
-        item {
-            Text(
-                text = "Last item",
-                style = TextStyle(color = MaterialTheme.colors.onBackground, fontSize = 36.sp),
-            )
+            CategoryCard()
+            CategoryCard()
+            CategoryCard()
         }
     }
+}
 
+@Composable
+fun CategoryCard() {
+    Card(
+        shape = MaterialTheme.shapes.small,
+        modifier = Modifier
+            .padding(
+                top = 2.dp,
+                start = 10.dp,
+                end = 10.dp
+            )
+            .fillMaxWidth(),
+        elevation = 8.dp,
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            ) {
+            Row() {
+                Column(
+                ) {
+                    Text(
+                        text = "Frutta e verdura",
+                        Modifier.padding(10.dp),
+                        style = TextStyle(color = MaterialTheme.colors.onBackground, fontSize = 25.sp),
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentSize(Alignment.CenterEnd)
+                ) {
+                    Icon(
+                        Icons.Rounded.KeyboardArrowDown,
+                        contentDescription = "Expand",
+                        modifier = Modifier
+                        .size(60.dp)
+                        .padding(5.dp)
+                    )
+                }
+            }
+        }
+    }
 }
