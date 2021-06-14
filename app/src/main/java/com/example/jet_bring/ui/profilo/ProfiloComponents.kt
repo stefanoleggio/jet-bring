469bbf1013bbd01da4181f2a93a2248fff7f55fd
Paolo <paos9904@gmail.com>
2021-06-14 15:43:19 +0200

    commenting

--- app/src/main/java/com/example/jet_bring/ui/profilo/ProfiloComponents.kt
+++ app/src/main/java/com/example/jet_bring/ui/profilo/ProfiloComponents.kt
@@ -1,38 +1,22 @@
 package com.example.jet_bring.ui.profilo
 
-import android.provider.Settings
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.MeasurePolicy
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.LayoutDirection
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.navigate
 import androidx.navigation.compose.rememberNavController
@@ -73,7 +57,6 @@ fun Table(
                     .fillMaxWidth()
                 )
             }
-
             boxesArgs.forEach() {
                 ClickableBox(title = it.title, navController = navController, route = it.link)
             }
@@ -111,7 +94,6 @@ fun Table(
             contentDescription = null)
         Spacer(modifier = Modifier.padding(LINE_SPACE))
         Text(text = title)
-        //
     }
 }
 
@@ -189,7 +171,8 @@ fun MyButton(bText: String,bClick:() -> Unit, icon: Int,modifier:Modifier) {
 }
 
 /**
- * Funzione componente che genera un pannello contenente un numero di RadialButton definito da
+ * Funzione componente che genera un pannello contenente un numero di RadialButton
+ * definito dal numero di elementi presenti nella lista passata come uno dei parametri
  */
 @Composable
 fun ChoosingTab(
@@ -207,6 +190,11 @@ fun ChoosingTab(
         }
         }
 }
+
+/**
+ * Funzione componente che genera una riga contenente un radio button clickable utilizzato in
+ * ChoosingTab per generare le righe di scelta
+ */
 @Composable
 fun RadioButtonRow(
     selected: Boolean,
@@ -228,6 +216,9 @@ fun RadioButtonRow(
     }
 }
 
+/**
+ * funzione per generare la preview di una Table generica
+ */
 @Composable
 @Preview
 fun TablePreview() {
@@ -245,23 +236,33 @@ fun TablePreview() {
     )
 }
 
+/**
+ * funzione per generare la preview di una clickableBox
+ */
 @Composable
 @Preview
 fun ClickableBoxPreview() {
     ClickableBox("Clickable Box Preview", rememberNavController(),"liste")
 }
+/**
+ * funzione per generare la preview di una clickableBox con icona
+ */
 @Composable
 @Preview
 fun ClickableBoxIconPreview() {
     ClickableBox("Clickable Box Preview", rememberNavController(),"liste",Icons.Default.Add)
 }
-
+/**
+ * funzione per generare la preview di una TwoButtonsRow
+ */
 @Composable
 @Preview
 fun TwoButtonsRowPreview() {
     TwoButtonsRow("Aspetto della lista", "miao", {},{}, R.drawable.ic_pixels, R.drawable.ic_half_circle)
 }
-
+/**
+ * funzione per generare la preview di una ChoosingTab
+ */
 @Composable
 @Preview
 fun ChoosingTabPreview() {
--- app/src/main/java/com/example/jet_bring/ui/profilo/ProfiloData.kt
+++ app/src/main/java/com/example/jet_bring/ui/profilo/ProfiloData.kt
@@ -1,39 +1,39 @@
 package com.example.jet_bring.ui.profilo
 
-import android.graphics.drawable.Drawable
-import android.media.Image
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.rounded.Email
-import androidx.compose.material.icons.rounded.Person
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.jet_bring.R
 
 
-
+/**
+ * data class utilizzata per il salvataggio dei dati utente
+ */
 data class UserData(
     var name: String = "John Doe",
     var email: String = "john_doe@gmail.com",
     var profileIcon: Int = R.drawable.ic_profile
 )
-
+/**
+ * data class utilizzata per i dati utilizzati per generare le tables in profilo
+ */
 data class TableData(
     val title: String,
     val iconTitle: ImageVector,
     val list: List<CBoxArg> = listOf()
 )
-
+/**
+ * data class utilizzata per gli elementi delle table
+ */
 data class CBoxArg(
     val title: String,
     val link: String
 )
-
+/**
+ * lista di dati utilizzata per generare le table in profilo
+ */
 /*TODO correggere le icone per farle corrispondere a quelle originali*/
 val tableDataList = listOf(
-    TableData("Impostazioni", Icons.Default.Settings),
-    /*
-    TableData("Informazioni su JetBring",Icons.Default.ThumbUp)
-    */
+    TableData("Impostazioni", Icons.Default.Settings)
 )
 
